b77d328d52a9573257dec91f7480660193f51ec3
kota-es <info@k-3110.com>
2019-10-03 16:57:27 +0900

    メッセージ非同期通信用のjsファイルを記述

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,51 @@
+$(function(){
+function buildHTML(message){
+  var image = ""
+    message.image ? image = `<img src="${message.image}">` : image = ""
+
+  var html =`<div class="message-box">
+              <div class="message-box__title">
+                <p class="message-box__title__username">
+                  ${message.user_name}
+                </p>
+                <p class="message-box__title__time">
+                  ${message.created_at}
+                </p>
+              </div>
+              <p class="message-box__message">
+                ${message.content}
+              </p>
+                ${image}
+             </div>`
+  return html
+}
+
+function ScrollToBottom(){
+  $('.main-view').animate({scrollTop: $('.main-view')[0].scrollHeight});
+}
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      $('.submit-btn').prop( 'disabled', false );
+      var html = buildHTML(data);
+      $('.main-view').append(html);      
+      $('.input-box__text').val("");      
+      ScrollToBottom();
+    })
+    .fail(function(){
+      $('.submit-btn').prop( 'disabled', false );
+      alert("エラー");
+    });
+  }); 
+});
